9a3bf55f7d39044bea00b5699afdebd9634e3b64
Nilkanth Bhalala <119674314+nilkanthbhalala@users.noreply.github.com>
2026-08-17 21:12:02 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,49 @@
+#include<unordered_map>
+#include<string>
+#include<algorithm>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        if(s.size() != t.size())
+            return false;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        for(int i = 0; i< s.size(); i++)
+        {
+            if(s[i] != t[i])
+                return false;
+        }
+        return true;
+    }
+};
+
+
+
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) 
+//     {
+//         if(s.size() != t.size())
+//             return false;
+
+//         unordered_map<char, int>mp;
+        
+//         for(char i: s)
+//             mp[i]++;
+        
+//         for(char i: t)
+//         {
+//             if(!mp[i])
+//                 return false;
+//             else
+//                 mp[i]--;
+//         }
+//         return true;
+
+//     }
+// };

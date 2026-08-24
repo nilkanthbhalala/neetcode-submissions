1698c3fe3ca39b7139432d292d75d7d5ddaba80c
Nilkanth Bhalala <119674314+nilkanthbhalala@users.noreply.github.com>
2026-08-24 22:44:48 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, int> mp;
+
+        for(const string &str: strs){
+            auto tempStr = str;
+            sort(tempStr.begin(), tempStr.end());
+            
+            if(mp.find(tempStr) == mp.end()){
+                mp[tempStr] = ans.size();
+                ans.push_back({str});
+            }
+            else{
+                ans[mp.find(tempStr) -> second].push_back(str);
+            }
+        }
+        return ans;
+    }
+};

2ab187665da7db2e856c0f5ed0357a39ccfd8a23
Nilkanth Bhalala <119674314+nilkanthbhalala@users.noreply.github.com>
2026-09-11 13:59:37 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n = temp.size();
+        vector<int> ans(n, 0);
+
+        for(int i = n-2; i>=0; --i){
+
+            int j = i+1;
+            while(temp[i] >= temp[j] && ans[j] != 0)
+                j += ans[j];
+
+            if(temp[i] < temp[j])
+                ans[i] = j - i;
+            else
+                ans[i] = 0;
+    
+        }
+        return ans;
+        
+    }
+};
